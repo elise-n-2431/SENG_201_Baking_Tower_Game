7becad8b3a6dc48c587e2dd679c202d41e21e358
hgr82 <hgr82@uclive.ac.nz>
2024-04-26 16:59:50 +1200

    Created Inventory class with some initial declared variables.

---
+++ src/main/java/seng201/team0/models/Inventory.java
@@ -0,0 +1,9 @@
+package seng201.team0.models;
+import java.util.ArrayList;
+
+public class Inventory {
+    private int bankBalance;
+    private ArrayList<String> storedItems;
+    private ArrayList<String> activeTowers;
+    private int lastSelected;
+}
