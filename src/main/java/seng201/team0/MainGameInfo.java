ce56199653b3f71236223cebfa67b6cbc0e51dc9
elise-n-2431 <elise.n.2431@gmail.com>
2024-05-03 13:35:14 +1200

    Created MainGameInfo to initialise and track global variables throughout the game

---
+++ src/main/java/seng201/team0/MainGameInfo.java
@@ -0,0 +1,32 @@
+package seng201.team0;
+
+public class MainGameInfo {
+    private Integer difficulty;
+    private String name;
+    private Integer numRounds;
+    private Integer currentRound;
+    private Integer remainingRounds;
+
+    public Integer getDifficulty(){
+        return difficulty;
+    }
+    public String getName(){
+        return name;
+    }
+    public Integer getNumRounds(){
+        return numRounds;
+    }
+    public Integer getCurrentRound(){
+        return currentRound;
+    }
+    public void updateRounds(){
+        currentRound += 1;
+        remainingRounds -= 1;
+        //should it do anything if numRounds exceeded?
+    }
+    public Integer getRemainingRounds(){
+        return remainingRounds;
+    }
+
+
+}
