e861a10fb6d765c38800d9f45da2614248246c0c
elise-n-2431 <elise.n.2431@gmail.com>
2024-05-05 19:09:27 +1200

    Created showinventorywindow and modified controller

--- src/main/java/seng201/team0/gui/ShopInventoryController.java
+++ src/main/java/seng201/team0/gui/ShopInventoryController.java
@@ -1,9 +1,20 @@
 package seng201.team0.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class ShopInventoryController {
+    /** Examples- to fill in with ids of elements
+     @FXML
+    private TextArea inputTextArea;
+    @FXML
+    private Label speakingLabel;
+     **/
 
-
-
-
-
+    @FXML
+    private void onSpeakClicked() {
+        /**speakingLabel.setText(inputTextArea.getText());
+        inputTextArea.setText("");**/
+    }
 }
