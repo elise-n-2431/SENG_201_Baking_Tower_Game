56571c326d546466b507578a1b0e3d5822f6ad81
elise-n-2431 <elise.n.2431@gmail.com>
2024-05-10 16:08:35 +1200

    added classes - not working

--- src/main/java/seng201/team0/gui/ShopInventoryController.java
+++ src/main/java/seng201/team0/gui/ShopInventoryController.java
@@ -3,14 +3,19 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import seng201.team0.models.Tower;
 
 public class ShopInventoryController {
-    /** Examples- to fill in with ids of elements
-     @FXML
+    public ShopInventoryController(Class Tower1){
+        Tower = Tower1;
+    }
+    private int selectedTowerIndex = -1;
+    private final Tower[] selectedTowers = new Tower[3];
+
+    @FXML
     private TextArea inputTextArea;
     @FXML
     private Label speakingLabel;
-     **/
 
     @FXML
     private void onSpeakClicked() {
