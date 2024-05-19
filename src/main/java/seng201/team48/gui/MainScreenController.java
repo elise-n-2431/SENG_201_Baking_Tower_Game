380b30f764ce02fc45a034891ebe0346c839f639
elise-n-2431 <elise.n.2431@gmail.com>
2024-05-20 11:36:06 +1200

    Timer bug fixed, progress bars added, towers are now buttons with graphics

--- src/main/java/seng201/team48/gui/MainScreenController.java
+++ src/main/java/seng201/team48/gui/MainScreenController.java
@@ -10,6 +10,7 @@ import seng201.team48.models.Bowl;
 import seng201.team48.models.Tower;
 import seng201.team48.services.BowlService;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,18 +18,33 @@ public class MainScreenController {
     public Button backButton;
     public Button shopButton;
     public Label moneyValue;
-    public ImageView imageOne;
-    public ImageView imageTwo;
-    public ImageView imageThree;
-    public ImageView imageFour;
-    public ImageView imageFive;
+    public Button imageOne;
+    public Button imageTwo;
+    public Button imageThree;
+    public Button imageFour;
+    public Button imageFive;
     public Label announcement;
     public Button resetButton;
+    public Button resetBowl;
     public Bowl currentBowl = null;
+    public Button recipeButton;
+    public ProgressBar reloadOne;
+    public ProgressBar reloadTwo;
+    public ProgressBar reloadThree;
+    public ProgressBar reloadFour;
+    public ProgressBar reloadFive;
     TowerManager towerManager;
     MainGameManager mainGameManager;
     BowlService bowlService = new BowlService();
     public AnimationTimer timer;
+    private boolean resetTimer = false;
+    private double reloadOneTemp = 0;
+    private double reloadTwoTemp = 0;
+    private double reloadThreeTemp = 0;
+    private double reloadFourTemp = 0;
+    private double reloadFiveTemp = 0;
+
+
 
     public MainScreenController(MainGameManager mainGameManager) {
         this.mainGameManager = mainGameManager;
@@ -36,47 +52,61 @@ public class MainScreenController {
 
         timer = new AnimationTimer() {
             private long bowlTime = 0;
+            private long reloadTimer = 0;
             @Override
             public void handle(long now) {
-
-                if (now - bowlTime >= 10_000_000_000L) {
+                if(bowlTime == 0){
+                    bowlTime = now;
+                    reloadTimer = now;
+                }
+                if (resetTimer){
+                    bowlTime = now;
+                    resetTimer = false;
+                }
+                if(now - reloadTimer >= 1_000_000_000L){
+                    increaseProgressBars();
+                    reloadTimer = now;
+                }
+                if (now - bowlTime >= 30_000_000_000L) {
                     this.stop();
                     fail();
                 }
-            }
-            public void restartTimer(long thisTime){
-                bowlTime = thisTime;
             }
 
         };
         timer.start();
     }
-    public void resetBowlTimer() {
-        //doesnt work :(
-        timer.restartTimer(System.nanoTime());
-    }
-    @FXML
-    private void updateValue(int increase) {
-        mainGameManager.setMoneyPerRound(increase);
-        String s = String.valueOf(increase);
-        moneyValue.setText(s);
-    }
-
 
     @FXML
     public void initialize(){
         List<Tower> playerTowers = towerManager.getPlayerTowers();
         String imagePath = "/images/"+playerTowers.get(0).getResourceType()+".png";
         Image imgOne = new Image(getClass().getResourceAsStream(imagePath));
-        imageOne.setImage(imgOne);
+        ImageView view = new ImageView(imgOne);
+        view.setFitHeight(100);
+        view.setPreserveRatio(true);
+        imageOne.setGraphic(view);
+        imageOne.setDisable(false); //disabled if not in use
+        reloadOne.setVisible(true);
 
         String imagePath2 = "/images/" +playerTowers.get(1).getResourceType()+ ".png";
         Image imgTwo = new Image(getClass().getResourceAsStream(imagePath2));
-        imageTwo.setImage(imgTwo);
+        ImageView view2 = new ImageView(imgTwo);
+        view2.setFitHeight(100);
+        view2.setPreserveRatio(true);
+        imageTwo.setGraphic(view2);
+        imageTwo.setDisable(false);
+        reloadTwo.setVisible(true);
 
         String imagePath3 = "/images/"+playerTowers.get(2).getResourceType()+".png";
         Image imgThree = new Image(getClass().getResourceAsStream(imagePath3));
-        imageThree.setImage(imgThree);
+        ImageView view3 = new ImageView(imgThree);
+        view3.setFitHeight(100);
+        view3.setPreserveRatio(true);
+        imageThree.setGraphic(view3);
+        imageThree.setDisable(false);
+        reloadThree.setVisible(true);
+
         /* prevents bank balance continuing to new rounds */
         if(mainGameManager.getCurrentRound()!=1){
             String s = String.valueOf(mainGameManager.getMoneyPerRound());
@@ -85,15 +115,81 @@ public class MainScreenController {
             mainGameManager.setMoneyPerRound(0);
         }
         Integer numSmall = mainGameManager.getNumSmall();
-        System.out.println(numSmall);
         Integer numLarge = mainGameManager.getNumLarge();
-        System.out.println(numLarge);
         if (numSmall != null){
             if (numLarge != null){
                 bowlService.setNumBowlsSelected(numSmall, numLarge);
             }
         }
+
+        /* testing for recipe book
+        List<Tower> allTowers = towerManager.getDefaultTowers();
+        List<Tower> towers = new ArrayList<Tower>(3);
+        towers.add(allTowers.get(0));
+        towers.add(allTowers.get(0));
+        towers.add(allTowers.get(1));
+        String product = inRecipe(towers);
+        if(product != null){
+            System.out.println(product);
+        } */
+
     }
+    public void increaseProgressBars() {
+        //get reload speed from somewhere - temp is same
+        reloadOne.setProgress(reloadOneTemp + 0.1);
+        reloadOneTemp += 0.1;
+        reloadTwo.setProgress(reloadTwoTemp + 0.1);
+        reloadTwoTemp += 0.1;
+        reloadThree.setProgress(reloadThreeTemp + 0.1);
+        reloadThreeTemp += 0.1;
+        reloadFour.setProgress(reloadFourTemp + 0.1);
+        reloadFourTemp += 0.1;
+        reloadFive.setProgress(reloadFiveTemp + 0.1);
+        reloadFiveTemp += 0.1;
+    }
+
+    public void resetBowlTimer() {
+        resetTimer = true;
+    }
+    @FXML
+    private void resetBowlClicked(){
+        //temporary way to test bowl timer
+        resetBowlTimer();
+    }
+    @FXML
+    private void updateValue(int increase) {
+        mainGameManager.setMoneyPerRound(increase);
+        String s = String.valueOf(increase);
+        moneyValue.setText(s);
+    }
+
+    @FXML
+    private void onOneClicked(){
+        reloadOne.setProgress(0);
+        reloadOneTemp = 0;
+    }
+    @FXML
+    private void onTwoClicked(){
+        reloadTwo.setProgress(0);
+        reloadTwoTemp = 0;
+    }
+    @FXML
+    private void onThreeClicked(){
+        reloadThree.setProgress(0);
+        reloadThreeTemp = 0;
+    }
+    @FXML
+    private void onFourClicked(){
+        reloadFour.setProgress(0);
+        reloadFourTemp = 0;
+    }
+    @FXML
+    private void onFiveClicked(){
+        reloadFive.setProgress(0);
+        reloadFiveTemp = 0;
+    }
+    //TESTING ENDS
+
     @FXML
     private void onBackClicked() {
         mainGameManager.closeMainScreenHome();
@@ -112,7 +208,7 @@ public class MainScreenController {
     }
     @FXML
     private void onResetClicked(){
-        //should not restart from start of track
+        //resets the bowl contents by creating a new bowl at same location
         currentBowl = bowlService.getNewBowl();
     }
     @FXML
@@ -130,9 +226,13 @@ public class MainScreenController {
             String product = inRecipe();
             if(product != null){
                 announcement.setText("You have made: " + product);
-                currentBowl = bowlService.getNewBowl();
-                //should put new bowl back to start of track
                 resetBowlTimer();
+                if(bowlService.getBowlsUsed()){
+                    //no more bowls
+                    endRound();
+                } else {
+                    currentBowl = bowlService.getNewBowl();
+                }
             } else {
                 announcement.setText("Sorry, that can't be baked, try again");
             }
